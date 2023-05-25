542bdbe63a54aeefbc9f5e6c30419f9a86365ced
rafaelvieiracosta <rvc.vieira@outlook.com>
2023-05-25 16:09:49 -0300

    feat: crud with express

---
+++ src/app.js
@@ -0,0 +1,50 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const livros = [
+  { id: 1, "titulo": "Livro 1" }, 
+  { id: 2, "titulo": "Livro 2" }
+];
+
+app.get('/', (req, res) => {
+  res.status(200).send('Home');
+});
+
+app.get('/livros', (req, res) => {
+  res.status(200).json(livros);
+});
+
+app.get('/livros/:id', (req, res) => {
+  let index = buscaLivro(req.params.id);
+  res.json(livros[index]);
+});
+
+app.post('/livros', (req, res) => {
+  livros.push(req.body);
+  res.status(201).send('Livro cadastrado com sucesso');
+});
+
+app.put('/livros/:id', (req, res) => {
+  let index = buscaLivro(req.params.id);
+  livros[index].titulo = req.body.titulo;
+  res.json(livros);
+});
+
+app.delete('/livros/:id', (req, res) => {
+  let { id } = req.params;
+  let index = buscaLivro(id);
+  let livroRemovido = livros.splice(index, 1);
+  res.send(`${livroRemovido[0].titulo} removido com sucesso`)
+});
+
+function buscaLivro(id) {
+  return livros.findIndex(livro => {
+    return livro.id == id;
+  })
+}
+
+
+export default app
