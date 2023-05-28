73ecf7d3d2d36731b26c9b85e06b7c0054771cf8
rafaelvieiracosta <rvc.vieira@outlook.com>
2023-05-28 20:04:42 -0300

    feat: new post books

--- src/app.js
+++ src/app.js
@@ -19,19 +19,9 @@ routes(app);
   { id: 2, "titulo": "Livro 2" }
 ]; */
 
-app.get('/livros', (req, res) => {
-  livros.find((err, livros) => {
-    res.status(200).json(livros);
-  });
-});
 app.get('/livros/:id', (req, res) => {
   let index = buscaLivro(req.params.id);
   res.json(livros[index]);
-});
-
-app.post('/livros', (req, res) => {
-  livros.push(req.body);
-  res.status(201).send('Livro cadastrado com sucesso');
 });
 
 app.put('/livros/:id', (req, res) => {
--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -8,6 +8,17 @@ class LivroController {
       });
     }
 
+    static cadastrarLivro = (req, res) => {
+      let livro = new livros(req.body);
+
+      livro.save((err) => {
+        if (err) {
+          res.status(500).send({ message: `${err.message} - falha ao cadastrar livro.`});
+        } else {
+          res.status(201).send(livro.toJSON());
+        }
+      })
+    }
 }
 
 export default LivroController
